7d16599176f23a5b6eed5ea97a6985044020bfab
Rahul218691 <rm218691@gmail.com>
2023-07-22 16:19:15 +0530

    updates

--- server/index.js
+++ server/index.js
@@ -2,16 +2,27 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
+const cookieParser = require('cookie-parser')
+
+const connectDB = require('./config/db')
 
 const app = express()
 
-app.use(cors())
+const corsOptions = {
+    credentials: true,
+    origin: process.env.CLIENT_BASE_URL
+}
+
+app.use(cors(corsOptions))
 app.use(helmet())
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
     console.log(`REST API RUNNING ON PORT: ${PORT}`)
+    connectDB()
 })
 
